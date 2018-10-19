4f91cd2edaf30d8ddebd28c33a96d31e671aa07f
etolejames <etolejames@gmail.com>
2018-10-19 11:29:02 +0300

    [Chore #161336289] Fix app creation error

--- store_app/__init__.py
+++ store_app/__init__.py
@@ -5,6 +5,5 @@ from Instance.config import configuration
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(configuration[config_name])
-    configuration[config_name].init_app(app)
 
     return app
