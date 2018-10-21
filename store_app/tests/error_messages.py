46004e75fc5e8a402a97f778014ee4290c8a338d
etolejames <etolejames@gmail.com>
2018-10-21 12:18:48 +0300

    [chore #161350147] correcctions to user module an its text 3

--- store_app/tests/error_messages.py
+++ store_app/tests/error_messages.py
@@ -4,11 +4,29 @@ from store_app.api.version1.errors import error_response
 class PaginationCase(unittest.TestCase):
     def test_error_response_feeback(self):
         message ='Bad request'
-        answ= error_response(404,message)
-        expected={"error":"Not Found","message":'Bad request'}
+        answ= error_response(400,message)
+        expected={"error":"Bad Request","message":'ERROR'}
 
         self.assertEqual(answ,expected,msg="Kurasa paginator not working as expected")
 
+        answ= error_response(404,message)
+        expected={"error":"Not Found","message":'ERROR'}
 
+        self.assertEqual(answ,expected,msg="Kurasa paginator not working as expected")
+
+        answ= error_response(401,message)
+        expected={"error":"Unauthorized","message":'ERROR'}
+
+        self.assertEqual(answ,expected,msg="Kurasa paginator not working as expected")
+
+        answ= error_response(403,message)
+        expected={"error":"Forbidden","message":'ERROR'}
+
+        self.assertEqual(answ,expected,msg="Kurasa paginator not working as expected")
+
+        answ= error_response(405,message)
+        expected={"error":"Method Not Allowed","message":'ERROR'}
+
+        self.assertEqual(answ,expected,msg="Kurasa paginator not working as expected")
 if __name__ == "__main__":
     unittest.main(verbosity=2)
