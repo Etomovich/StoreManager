2ea5c13ea4e2f20ff90e4abae5be446580fb576b
etolejames <etolejames@gmail.com>
2018-10-21 13:50:09 +0300

    [chore #161350147] corrections to user module an its tests 4

--- store_app/tests/error_messages.py
+++ store_app/tests/error_messages.py
@@ -7,26 +7,33 @@ class PaginationCase(unittest.TestCase):
         answ= error_response(400,message)
         expected={"error":"Bad Request","message":'ERROR'}
 
-        self.assertEqual(answ,expected,msg="Kurasa paginator not working as expected")
+        self.assertEqual(answ,expected,msg="error_responce not working properly")
 
         answ= error_response(404,message)
         expected={"error":"Not Found","message":'ERROR'}
 
-        self.assertEqual(answ,expected,msg="Kurasa paginator not working as expected")
+        self.assertEqual(answ,expected,msg="error_responce not working properly")
 
         answ= error_response(401,message)
         expected={"error":"Unauthorized","message":'ERROR'}
 
-        self.assertEqual(answ,expected,msg="Kurasa paginator not working as expected")
+        self.assertEqual(answ,expected,msg="error_responce not working properly")
 
         answ= error_response(403,message)
         expected={"error":"Forbidden","message":'ERROR'}
 
-        self.assertEqual(answ,expected,msg="Kurasa paginator not working as expected")
+        self.assertEqual(answ,expected,msg="error_responce not working properly")
 
         answ= error_response(405,message)
         expected={"error":"Method Not Allowed","message":'ERROR'}
 
-        self.assertEqual(answ,expected,msg="Kurasa paginator not working as expected")
+        self.assertEqual(answ,expected,msg="error_responce not working properly")
+
+    def test_unknown_error(self):
+        message ='ERROR'
+        answ= error_response(2,message)
+        expected={"error":"Unknown error","message":'ERROR'}
+
+        self.assertEqual(answ,expected,msg="error_responce not working properly")
 if __name__ == "__main__":
     unittest.main(verbosity=2)
