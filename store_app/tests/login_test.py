98fb36a662f7ea52a1ee2d0d30f03ea9bc3e0b2b
etolejames <etolejames@gmail.com>
2018-10-21 13:15:19 +0300

    [chore #161350147] corrections to user module an its tests 3

--- store_app/tests/login_test.py
+++ store_app/tests/login_test.py
@@ -54,9 +54,9 @@ class LoginTests(unittest.TestCase):
     def test_registration_with_valid_credentials(self):
         '''Tests that a user is registered successfully'''
 
-        access = self.get_admin_access()
-        response = self.client.post('/api/v1/admin/users', data=self.user_1,headers=access,content_type='application/json')
-        self.assertEqual(response.get_data, "")
+        self.client.environ_base['Authorization'] = self.get_admin_access()
+        response = self.client.post('/api/v1/admin/users', data=self.user_1,content_type='application/json')
+        self.assertEqual(response.get_data(), "")
 
         #self.assertEqual(response.status_code, 201)
 
